ebcbf76b7de45d7887f83fe056c91b43cae5e762
Blackbaud-MitchellDunaway <mitchell.dunaway@blackbaud.com>
2014-02-07 13:19:57 -0600

    Feb 7, 2014

---
+++ js/reus_wrpr_giving_2014_js_scripts.js
@@ -0,0 +1,42 @@
+[[U8:text/js]]
+// reus_wrpr_giving_2014_js_scripts
+jQuery(document).ready(function($) {
+
+
+
+	function panel_show(elements){
+		elements.addClass('active');
+		$('body').bind('click touchstart',function(e){
+			if (!$(e.target).closest(elements).length) {
+				panel_hide(elements);
+			}
+		});
+	}
+
+	function panel_hide(elements){
+		elements.removeClass('active');
+		$('body').unbind('click touchstart');
+	}
+
+	$('#sign_up_button, #sign_up_close').click(function(e){
+		e.preventDefault();
+		var elements = $('#sign_up_button, #sign_up form');
+		if (elements.hasClass('active')) {
+			panel_hide(elements);
+		} else {
+			panel_show(elements);
+		}
+	})
+
+	$('#nav_reveal').click(function(e){
+		var elements = $('#nav_panel, #nav_reveal');
+		if (elements.hasClass('active')) {
+			panel_hide(elements);		
+		} else {			
+			e.preventDefault();
+			panel_show(elements);
+		}
+	})
+
+
+}); //end jQuery(document).ready();
